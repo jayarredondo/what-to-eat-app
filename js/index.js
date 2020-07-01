58f09016473564b3e584716fdb7f6738eda63650
Jay Arredondo <jearredondo13@gmail.com>
2020-07-01 17:34:16 -0500

    made a few changes in styling

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ let foodOptions = [
     {type: "drive-thru", name: "Taco Bell", specialty: "burritos"},
     {type: "drive-thru", name: "Panda Express", specialty: "asian cuisine"},
     {type: "drive-thru", name: "Busch\'s Chicken", specialty: "chicken"},
-    {type: "drive-thru", name: "Taco Cabana", specialty: "mexican food"},
+    {type: "drive-thru", name: "Taco Cabana", specialty: "mexican-food"},
     {type: "drive-thru", name: "Churches Chicken", specialty: "chicken"},
     {type: "drive-thru", name: "Popeye\'s", specialty: "chicken"},
     {type: "drive-thru", name: "KFC", specialty: "chicken"},
@@ -25,56 +25,56 @@ let foodOptions = [
     {type: "drive-thru", name: "Little Caesar\'s", specialty: "pizza"},
     {type: "drive-thru", name: "In-N-Out", specialty: "burgers"},
     {type: "drive-thru", name: "Carl\'s Jr.", specialty: "burgers"},
-    {type: "drive-thru", name: "Las Palapas", specialty: "mexican food"},
-    {type: "drive-thru", name: "Taco Palenque", specialty: "tacos"},
-    {type: "drive-thru", name: "Mama Margie's", specialty: "mexican food"},
+    {type: "drive-thru", name: "Las Palapas", specialty: "mexican-food"},
+    {type: "drive-thru", name: "Taco Palenque", specialty: "mexican-food"},
+    {type: "drive-thru", name: "Mama Margie's", specialty: "mexican-food"},
     {type: "order-to-go", name: "La Madeleine", specialty: "sandwiches"},
-    {type: "order-to-go", name: "Pizza Hut", specialty: "pizza", phoneNumber: ""},
-    {type: "order-to-go", name: "Domino\'s", specialty: "pizza", phoneNumber: ""},
-    {type: "order-to-go", name: "Papa John\'s", specialty: "pizza", phoneNumber: ""},
-    {type: "order-to-go", name: "Wing Stop", specialty: "wings", phoneNumber: ""},
-    {type: "order-to-go", name: "Plucker\'s", specialty: "wings", phoneNumber: ""},
-    {type: "order-to-go", name: "Wing Daddy\'s", specialty: "wings", phoneNumber: ""},
-    {type: "order-to-go", name: "Goomba\'s Pizzeria", specialty: "pizza", phoneNumber: ""},
-    {type: "order-to-go", name: "Oblate Cafe", specialty: "mexican food", phoneNumber: ""},
-    {type: "order-to-go", name: "Panchito\'s", specialty: "mexican food", phoneNumber: ""},
-    {type: "order-to-go", name: "Rusty Bucket\'s Jaw-Smacking BBQ", specialty: "BBQ", phoneNumber: ""},
-    {type: "order-to-go", name: "Rudy's BBQ", specialty: "BBQ", phoneNumber: ""},
-    {type: "order-to-go", name: "Freebird's", specialty: "burritos", phoneNumber: ""},
-    {type: "order-to-go", name: "Chipotle", specialty: "burritos", phoneNumber: ""},
-    {type: "order-to-go", name: "Blanco BBQ", specialty: "BBQ", phoneNumber: ""},
-    {type: "dine-in", name: "Chili's Grill & Bar", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "J. Alexander's Restaurant", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "Cheddar's Scratch Kitchen", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "Twin Peaks", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "Walk-Ons", specialty: "wings", phoneNumber: ""},
-    {type: "dine-in", name: "Guillermo's", specialty: "mexican-food", phoneNumber: ""},
-    {type: "dine-in", name: "Applebee's", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "T.G.I. Friday's", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "Kona Grill", specialty: "asian cuisine", phoneNumber: ""},
-    {type: "dine-in", name: "Republic of Texas", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "Iron Cactus Mexican Restaurant and Margarita Bar", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "Paesanos Rivervalk", specialty: "italian", phoneNumber: ""},
-    {type: "dine-in", name: "Olive Garden", specialty: "italian", phoneNumber: ""},
-    {type: "dine-in", name: "Macaroni Grill", specialty: "italian", phoneNumber: ""},
-    {type: "dine-in", name: "Luciano's Neighborhood Pizzeria", specialty: "italian", phoneNumber: ""},
-    {type: "dine-in", name: "Acenar Mexican Restaurant", specialty: "mexican-food", phoneNumber: ""},
-    {type: "dine-in", name: "Bourbon Streen Seafood Kitchen Downtown", specialty: "seafood", phoneNumber: ""},
-    {type: "dine-in", name: "Red Lobster", specialty: "seafood", phoneNumber: ""},
-    {type: "dine-in", name: "Sea Island", specialty: "seafood", phoneNumber: ""},
-    {type: "dine-in", name: "Ostra Restaurant", specialty: "seafood", phoneNumber: ""},
-    {type: "dine-in", name: "Tomatillo's", specialty: "mexican-food", phoneNumber: ""},
-    {type: "dine-in", name: "Rosario's", specialty: "tacos", phoneNumber: ""},
-    {type: "dine-in", name: "Dick's Last Resort", specialty: "general", phoneNumber: ""},
-    {type: "dine-in", name: "Jim's", specialty: "breakfast", phoneNumber: ""},
-    {type: "dine-in", name: "Denny's", specialty: "breakfast", phoneNumber: ""},
-    {type: "dine-in", name: "IHOP", specialty: "breakfast", phoneNumber: ""},
-    {type: "dine-in", name: "Waffle House", specialty: "breakfast", phoneNumber: ""},
-    {type: "dine-in", name: "Fogo de Chao Brazilian Steakhouse", specialty: "tacos", phoneNumber: ""},
-    {type: "dine-in", name: "Mi Tierra Cafe y Panaderia", specialty: "mexican-food", phoneNumber: ""},
-    {type: "dine-in", name: "Pico De Gallo", specialty: "mexican-food", phoneNumber: ""},
-    {type: "dine-in", name: "Margaritaville", specialty: "seafood", phoneNumber: ""},
-    {type: "dine-in", name: "Hooters", specialty: "wings", phoneNumber: ""}
+    {type: "order-to-go", name: "Pizza Hut", specialty: "pizza"},
+    {type: "order-to-go", name: "Domino\'s", specialty: "pizza"},
+    {type: "order-to-go", name: "Papa John\'s", specialty: "pizza"},
+    {type: "order-to-go", name: "Wing Stop", specialty: "wings"},
+    {type: "order-to-go", name: "Plucker\'s", specialty: "wings"},
+    {type: "order-to-go", name: "Wing Daddy\'s", specialty: "wings"},
+    {type: "order-to-go", name: "Goomba\'s Pizzeria", specialty: "pizza"},
+    {type: "order-to-go", name: "Oblate Cafe", specialty: "mexican-food"},
+    {type: "order-to-go", name: "Panchito\'s", specialty: "mexican-food"},
+    {type: "order-to-go", name: "Rusty Bucket\'s Jaw-Smacking BBQ", specialty: "BBQ"},
+    {type: "order-to-go", name: "Rudy's BBQ", specialty: "BBQ"},
+    {type: "order-to-go", name: "Freebird's", specialty: "burritos"},
+    {type: "order-to-go", name: "Chipotle", specialty: "burritos"},
+    {type: "order-to-go", name: "Blanco BBQ", specialty: "BBQ"},
+    {type: "dine-in", name: "Chili's Grill & Bar", specialty: "bar-grill"},
+    {type: "dine-in", name: "J. Alexander's Restaurant", specialty: "bar-grill"},
+    {type: "dine-in", name: "Cheddar's Scratch Kitchen", specialty: "bar-grill"},
+    {type: "dine-in", name: "Twin Peaks", specialty: "bar-grill"},
+    {type: "dine-in", name: "Walk-Ons", specialty: "wings"},
+    {type: "dine-in", name: "Guillermo's", specialty: "mexican-food"},
+    {type: "dine-in", name: "Applebee's", specialty: "bar-grill"},
+    {type: "dine-in", name: "T.G.I. Friday's", specialty: "bar-grill"},
+    {type: "dine-in", name: "Kona Grill", specialty: "asian cuisine"},
+    {type: "dine-in", name: "Republic of Texas", specialty: "bar-grill"},
+    {type: "dine-in", name: "Iron Cactus Mexican Restaurant and Margarita Bar", specialty: "mexican-food"},
+    {type: "dine-in", name: "Paesanos Rivervalk", specialty: "italian"},
+    {type: "dine-in", name: "Olive Garden", specialty: "italian"},
+    {type: "dine-in", name: "Macaroni Grill", specialty: "italian"},
+    {type: "dine-in", name: "Luciano's Neighborhood Pizzeria", specialty: "italian"},
+    {type: "dine-in", name: "Acenar Mexican Restaurant", specialty: "mexican-food"},
+    {type: "dine-in", name: "Bourbon Streen Seafood Kitchen Downtown", specialty: "seafood"},
+    {type: "dine-in", name: "Red Lobster", specialty: "seafood"},
+    {type: "dine-in", name: "Sea Island", specialty: "seafood"},
+    {type: "dine-in", name: "Ostra Restaurant", specialty: "seafood"},
+    {type: "dine-in", name: "Tomatillo's", specialty: "mexican-food"},
+    {type: "dine-in", name: "Rosario's", specialty: "mexican-food"},
+    {type: "dine-in", name: "Dick's Last Resort", specialty: "bar-grill"},
+    {type: "dine-in", name: "Jim's", specialty: "breakfast"},
+    {type: "dine-in", name: "Denny's", specialty: "breakfast"},
+    {type: "dine-in", name: "IHOP", specialty: "breakfast"},
+    {type: "dine-in", name: "Waffle House", specialty: "breakfast"},
+    {type: "dine-in", name: "Fogo de Chao Brazilian Steakhouse", specialty: "mexican-food"},
+    {type: "dine-in", name: "Mi Tierra Cafe y Panaderia", specialty: "mexican-food"},
+    {type: "dine-in", name: "Pico De Gallo", specialty: "mexican-food"},
+    {type: "dine-in", name: "Margaritaville", specialty: "seafood"},
+    {type: "dine-in", name: "Hooters", specialty: "wings"}
 ];
 
 let dessertOptions = [
@@ -105,6 +105,10 @@ let dessertOptions = [
     {name: "La Chiquita Paleteria y Neveria"},
 ];
 
+var type = document.getElementById('type').value;
+var specialty = document.getElementById('specialty').value;
+
+
 
 // FUNCTIONS TO RENDER FOOD SPECS
 
@@ -123,11 +127,15 @@ function randomDessertOption(dessertArray) {
 // DOM MANIPULATION
 
 // ====GENERATE RANDOM FOOD, NO SPECS=====
-document.getElementById('randomFoodButton').addEventListener('click', function () {
-    document.getElementById('results').innerHTML = `<br><br><h2>How about... </h2><h1>${randomFoodOption()}!</h1>`;
-});
 
-// =====GENERATE RANDOM FAST FOOD BASED ON USER INPUT=====
+function generateRandomFood(){
+    document.getElementById('randomFoodButton').addEventListener('click', function () {
+        document.getElementById('results').innerHTML = `<br><br><h2 class="center">How about... </h2><h1 class="center">${randomFoodOption()}!</h1>`;
+    })
+}
+generateRandomFood();
+
+// =====GENERATE RANDOM BASED ON USER INPUT=====
 document.getElementById('narrow-search').addEventListener('click', function () {
     var type = document.getElementById('type').value;
     var specialty = document.getElementById('specialty').value;
@@ -138,9 +146,13 @@ document.getElementById('narrow-search').addEventListener('click', function () {
             return food.type === type && food.specialty === specialty
         }
     });
-    document.getElementById('results').innerHTML = `<br><br><h2>Based on your search, how about:</h2><h1>${result[Math.floor(Math.random() * result.length)].name}</h1>`;
+    console.log(result);
+    document.getElementById('results').innerHTML = `<br><br><h2 class="center">Based on your search, how about:</h2><h1 class="center">${result[Math.floor(Math.random() * result.length)].name}</h1>`;
 });
 
+// ======POPULATE INPUT FIELD FOR TYPE OF FOOD BASED ON CATEGORY=======
+
+
 document.getElementById('getDessert').addEventListener('click', function(){
-    document.getElementById('dessertResults').innerHTML = `<br><br><h2>How about... </h2><h1>${randomDessertOption()}!</h1>`;
+    document.getElementById('results').innerHTML = `<br><br><h2 class="center">How about... </h2><h1 class="center">${randomDessertOption()} for dessert?</h1>`;
 });
